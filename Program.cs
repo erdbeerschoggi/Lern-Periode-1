57e0c45278ce254a4f4ebe3efdcef95e90803bc4
erdbeerschoggi <M.Piotrowska.inf24@stud.bbbaden.ch>
2024-09-06 10:31:43 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace KonsolenTicTacToe
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 2 3");
+            Console.WriteLine("4 5 6");
+            Console.WriteLine("7 8 9");
+            
+            
+
+            Console.WriteLine("Wo möchtest Du Dein Kreuz setzen?");
+
+            { 
+            
+            }
+
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("1 2 3");
+            Console.WriteLine("4 5 6");
+            Console.WriteLine("7 8 9");
+
+        }
+    }
+}
